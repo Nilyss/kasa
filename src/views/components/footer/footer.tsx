d559910b93af526b4f93a6707c67017e26971571
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2023-04-14 15:23:45 +0200

    starting footer integration
    footer.tsx :
    
    — import logo from assets

--- src/views/components/footer/footer.tsx
+++ src/views/components/footer/footer.tsx
@@ -1,4 +1,27 @@
-import './footer.scss'
-export default function Footer() {
-    return <><p>Footer component works!</p></>
+import "./footer.scss";
+
+// assets
+import logo from "../../../assets/images/logo_white.png";
+export default function Footer(): JSX.Element {
+  const whiteLogo: string = logo;
+  const credentials: string = "© 2020 Kasa. All rights reserved";
+
+  return (
+    <footer className={"footerContainer"}>
+      <div className={"footerContainer__containerTop"}>
+        <figure className={"footerContainer__containerTop__imageWrapper"}>
+          <img
+            className={"footerContainer__containerTop__imageWrapper__image"}
+            src={whiteLogo}
+            alt="branding logo"
+          />
+        </figure>
+      </div>
+      <div className={"footerContainer__containerBottom"}>
+        <p className={"footerContainer__containerBottom__message"}>
+            {credentials}
+        </p>
+      </div>
+    </footer>
+  );
 }
